9c8409d1cb7b2a26193155cdce60339552792df3
Alpha-[REDACTED] <135540225+Redax6996@users.noreply.github.com>
2023-06-05 21:20:26 +0530

    For removing bloatware.

---
+++ bloatware.bat
@@ -0,0 +1,41 @@
+@echo off
+setlocal enabledelayedexpansion
+
+echo Listing currently installed apps (*DO NOT UNINSTALL ANY APP, IF YOU AREN'T SURE WHAT IT DOES.*)...
+echo --------------------------------------------------------
+
+:: Get the list of preinstalled apps
+for /f "tokens=2,*" %%A in ('reg query "HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall" /s ^| findstr DisplayName') do (
+    set "appKey=%%A"
+    set "appDisplayName=%%B"
+
+    :: Check if the app is necessary for system functionality
+    reg query "HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall\!appKey!" /v SystemComponent >nul 2>&1
+    if errorlevel 1 (
+        echo [!appKey!] !appDisplayName!
+    )
+)
+
+echo --------------------------------------------------------
+echo.
+
+set "uninstallApps="
+set /p "uninstallApps=Enter the app(s) you want to uninstall (comma-separated, leave blank to exit): "
+
+:: Check if the user provided any apps to uninstall
+if not "%uninstallApps%"=="" (
+    :: Split the input into individual app names
+    for %%A in ("%uninstallApps:,=" "%") do (
+        :: Uninstall each app
+        echo Uninstalling %%~A...
+        start /wait "" powershell.exe -Command "Get-WmiObject -Query \"SELECT * FROM Win32_Product WHERE Name LIKE '%%~A%%'\" | ForEach-Object { $_.Uninstall() }"
+        echo Uninstalled %%~A.
+        timeout 2 > nul
+        start "Disabling transparency effects, to improve performance." DisableTransparencyEffects.bat
+    )
+)
+
+echo No problem, moving on...
+timeout 2 > nul
+ start "Disabling transparency effects, to improve performance." DisableTransparencyEffects.bat
+
