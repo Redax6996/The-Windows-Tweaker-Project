ef6a3f222bbea50e25bd09563214eeaadf011992
Alpha-[REDACTED] <135540225+Redax6996@users.noreply.github.com>
2023-06-10 18:58:13 +0530

    Update ultimateperformance.bat

--- ultimateperformance.bat
+++ ultimateperformance.bat
@@ -8,6 +8,7 @@ powercfg -setactive e9a42b02-d5df-448d-aa00-03f14749eb61
 
 echo The Ultimate Performance power plan has been enabled.
 echo Note: Your PC will now consume more electricity than before, in order to improve performance. If you do not want that, you can undo this tweak later.
+echo Moving on...please do not exit this window.
 
 timeout 15 > nul
 start "Final optimization 1" finalOpti1.bat
